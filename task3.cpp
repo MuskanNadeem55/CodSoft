0114a0ec1c821a28abf2aad657910d77a24bd6d3
MuskanNadeem55 <160000879+MuskanNadeem55@users.noreply.github.com>
2024-05-08 07:22:54 -0700

    Add files via upload

---
+++ task3.cpp
@@ -0,0 +1,36 @@
+//Number guessing game***
+#include <iostream>
+
+#include <ctime>
+
+int main()
+      {
+        std::cout << "***Welcome to Number Guessing Game***\n";
+
+        std::cout << "Enter guess limit: ";
+        int lim; std::cin >> lim;
+        srand( time(0) ^ clock() );
+        const int num = rand() % lim;
+        while (true)
+        {
+        std::cout << "Enter your guess: ";
+        int guess; std::cin >> guess;
+        if (guess < num) 
+        {
+            std::cout << "Your guess is too small, please retry\n";
+            continue;
+        } 
+        else if (guess > num)
+        {
+            std::cout << "Your guess is too large, please retry\n";
+            continue;
+        } 
+        else 
+        {
+            std::cout << "You have guessed correctly!\n";
+            break;
+        }
+
+    }
+    return 0;
+}
